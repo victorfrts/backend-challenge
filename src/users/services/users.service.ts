6bc5b523a6f5c0a47deb0f833354ccb4f7e081f3
Victorfrts <victor.pe.freitas@gmail.com>
2022-01-11 15:57:19 -0400

    add fetch cep

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import axios from 'axios';
 import { Repository } from 'typeorm';
 import { User } from '../entities/Users.entity';
+
 
 @Injectable()
 export class UsersService {
@@ -20,7 +22,11 @@ export class UsersService {
 
   async create(body: any) {
     const User = await this.UsersRepo.findOne(body.cpf);
-    if(User.cpf === undefined){
+    const response = await axios.get(`https://viacep.com.br/ws/${body.cep}/json/`)
+    body.logradouro = response.data.logradouro
+    body.cidade = response.data.localidade
+    body.estado = response.data.uf
+    if(User === undefined){
       const newUser = this.UsersRepo.create(body);
       return this.UsersRepo.save(newUser);
     }else{
